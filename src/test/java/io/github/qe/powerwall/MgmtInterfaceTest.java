0a71e956f496375e842659e057e4ca13ff079358
Lewin Chan <8480608+quotidian-ennui@users.noreply.github.com>
2024-07-04 21:07:13 +0100

    test: add tests for management interface

---
+++ src/test/java/io/github/qe/powerwall/MgmtInterfaceTest.java
@@ -0,0 +1,53 @@
+package io.github.qe.powerwall;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class MgmtInterfaceTest {
+  @TestHTTPResource(value = "/management", management = true)
+  URL mgmtUrl;
+  @Inject
+  StatsCollector service;
+
+  @Test
+  void testMgmtLoginInfo() throws Exception {
+    String expected =
+      String.format(
+        "%s://%s:%d/info/loginStatus",
+        mgmtUrl.getProtocol(), mgmtUrl.getHost(), mgmtUrl.getPort());
+    service.login();
+    System.err.println(mgmtUrl);
+    System.err.println(expected);
+    URL targetURL = URI.create(expected).toURL();
+    try (InputStream in = targetURL.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      assertThat(contents, Matchers.containsString("loggedin"));
+    }
+  }
+
+  @Test
+  void testMgmtNetworks() throws Exception {
+    String expected =
+        String.format(
+            "%s://%s:%d/info/networks",
+            mgmtUrl.getProtocol(), mgmtUrl.getHost(), mgmtUrl.getPort());
+    service.login();
+    System.err.println(mgmtUrl);
+    System.err.println(expected);
+    URL targetURL = URI.create(expected).toURL();
+    try (InputStream in = targetURL.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      assertThat(contents, Matchers.containsString("ethernet_tesla_internal_default"));
+    }
+  }
+}
