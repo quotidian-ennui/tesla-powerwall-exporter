4b9b8ae3a87ca8215d533f81a87b59949f3b3436
Lewin Chan <8480608+quotidian-ennui@users.noreply.github.com>
2025-02-05 21:07:57 +0000

    test: disable mgmt test on github-ci

--- src/test/java/io/github/qe/powerwall/MgmtInterfaceTest.java
+++ src/test/java/io/github/qe/powerwall/MgmtInterfaceTest.java
@@ -2,8 +2,10 @@ package io.github.qe.powerwall;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import io.github.qe.powerwall.Profiles.Standard;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,14 +13,17 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 @QuarkusTest
+@TestProfile(Standard.class)
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 class MgmtInterfaceTest {
   @TestHTTPResource(value = "/management", management = true)
   URL mgmtUrl;
 
   @Inject PowerwallService service;
-  @Inject ManagementInfo mgmtInfo;
+  @Inject ManagementInfo info;
 
   @Test
   void testMgmtLoginInfo() throws Exception {
